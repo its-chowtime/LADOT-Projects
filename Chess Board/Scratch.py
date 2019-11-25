f1d08e6e88670286a63e514d2501a8d451d275f2
Patrick Chau <406822@dot.cw.ci.la.ca.us>
2019-11-25 14:14:56 -0800

    Created a move function

--- Chess Board/Scratch.py
+++ Chess Board/Scratch.py
@@ -3,9 +3,6 @@
 import numpy as np
 import random
 
-cols =['1','2','3','4','5','6','7','8']
-rows = ['A','B','C','D','E','F','G','H']
-
 cols_alpha_index = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7}
 rows_index_alpha = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H'}
 
@@ -18,24 +15,21 @@ c_b = [['A1','A2','A3','A4','A5','A6','A7','A8'],
        ['F1','F2','F3','F4','F5','F6','F7','F8'],
        ['G1','G2','G3','G4','G5','G6','G7','G8'],
        ['H1','H2','H3','H4','H5','H6','H7','H8']]
-
-
-# Visual chessboard with letters - this is what is showing up in the blackbox
-for this in range(8):
-    for that in range(8):
-        all_rows = rows[this] + cols[that]
-        print(all_rows, end =' ')
+'''
+for x in c_b:
     print()
+    print(x)
+'''
+# Place knight in starting position
 
-# Chessboard with 0 values
+knight = 'Kn'
+c_b[0][0] = knight
+print(c_b)
+'''
+for x in c_b:
+    c_b[0][0] = 'Kn'
+    print()
+    print(x)
 
-# Function - 2D array
-
-def cb_array():
-    arr = []
-    row,col = (8,8)
-    arr=[[0]*col]*row
-    for row in arr:
-        print(row)
-    return arr
-print(cb_array())
+def move():
+'''
