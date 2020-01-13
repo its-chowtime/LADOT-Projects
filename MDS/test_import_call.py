14e6ad272d91f64f1c1676d0254b1a263a0348bc
Patrick Chau <406822@dot.cw.ci.la.ca.us>
2020-01-13 13:30:17 -0800

    Finished first working dash

--- MDS/test_import_call.py
+++
@@ -1,4 +0,0 @@
-import os
-# os.system('python C:/Users/406822/Desktop/LADOT-Projects/MDS/MDS_Feed_Live_1.1.py')
-os.system('python C:/Users/406822/Desktop/LADOT-Projects/MDS/MDS_Feed_Live_1.1.py')
-os.system('python C:/Users/406822/Desktop/LADOT-Projects/MDS/MDS_Feed_Live_1.1.py')
